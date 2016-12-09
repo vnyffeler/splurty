cb76f5486ad154d1136e18f5b228213b985072f8
Vicky Nyffeler <vnyffeler@gmail.com>
2016-12-09 01:40:14 +0000

    Updated the create button to create a row in the db and redirect to the root page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,5 +7,16 @@ class QuotesController < ApplicationController
     @quote= Quote.new
   end
 
+  def create 
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+
 
 end
